291dfe160b91b8f87769f13a3dd7c9df4b5fcf6c
Trapp-m <pascal@dohogne.be>
2021-10-13 16:16:46 +0200

    test

---
+++ delete_old_files_sql/change_date.TempPoint.ps1
@@ -0,0 +1,16 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.194
+	 Created on:   	13/10/2021 15:45
+	 Created by:   	pdohogne
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
+foreach ($file in Get-ChildItem "D:\Temp\mosart_03.04") { $(Get-Item $file.Fullname).lastwritetime = $(Get-Date).AddHours(-5) }
---
+++ delete_old_files_sql/delete_old_files_sql.TempPoint.ps1
@@ -0,0 +1,38 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.194
+	 Created on:   	13/10/2021 15:37
+	 Created by:   	pdohogne
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		Delete sql backup file older than x days
+#>
+
+#----- Define parameters -----#
+#----- Get current date ----#
+$Now = Get-Date
+$Days = "15" #----- define amount of days ----#
+$Targetfolder = "D:\Temp\mosart_03.04" #----- define folder where files are located ----#
+$Extension = "*.xml" #----- define extension ----#
+$Lastwrite = $Now.AddDays(-$Days)
+
+#----- Get files based on lastwrite filter and specified folder ---#
+$Files = Get-Children $Targetfolder -include $Extension -Recurse | where { $_.LastwriteTime -le "$Lastwrite" }
+
+foreach ($File in $Files)
+{
+	if ($File -ne $Null)
+	{
+		write-host "Deleting File $File" backgroundcolor "DarkRed"
+		Remove-item $File.Fullname | out-null
+	}
+	else{
+		write-host "No more files to delete" -forgroundcolor "Green"
+	}
+	
+}
+
+
